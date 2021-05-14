581b3a41162f2230d498ea304b29a69ed04a8df7
JJ <jaroslaw.janikowski@gmail.com>
2021-05-14 13:24:28 +0200

    Dodanie uruchamiania plików wideo przez streamlink

--- news.py
+++ news.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import sqlite3
-from pathlib import Path
-import html.parser
+import threading
+import subprocess
 import webbrowser
+import html.parser
 import tkinter as tk
 from tkinter import ttk
+from pathlib import Path
 from tkinter.scrolledtext import ScrolledText
 
 
@@ -16,6 +18,35 @@ style = {
         'font': ('roboto', 16, 'normal')
     }
 }
+
+
+class StreamlinkViewer(threading.Thread):
+    def __init__(self, url, quality='worst', **kwargs):
+        super().__init__()
+
+        # self.daemon = True  # powoduje dziwne zawieszenia po kilku odtworzeniach filmów
+
+        self._url = url
+        self._quality = quality
+
+        self._on_start = kwargs['on_start'] if 'on_start' in kwargs else None
+
+    def run(self):
+        self._run_streamlink(self._url, self._quality)
+
+    def _run_streamlink(self, url, quality='worst'):
+        # run process in blocking mode
+        with subprocess.Popen(['streamlink', url, quality, '-p', 'mpv'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL) as p:
+            while p.stdout:
+                # read lines from stdout
+                l = p.stdout.readline()
+                if l == b'':
+                    break
+
+                # if certain line is detected then stop blocking main window
+                if b'Writing stream to output' in l:
+                    if callable(self._on_start):
+                        self._on_start()
 
 
 class NewsParser(html.parser.HTMLParser):
@@ -42,6 +73,32 @@ class NewsParser(html.parser.HTMLParser):
         return self._text.strip()
 
 
+class WaitDialog(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.withdraw()
+        self.title('Proszę czekać...')
+        self.geometry('250x120')
+        self.transient(master)
+        self.bind('<Escape>', self._on_escape)
+
+        label = tk.Label(self, text='Operacja w toku, proszę czekać...')
+        label.pack(side=tk.TOP, fill=tk.X, expand=True)
+
+    def _on_escape(self, event=None):
+        self.hide()
+
+    def show(self):
+        self.deiconify()
+        self.wait_visibility()
+        self.grab_set()
+        # self.wait_window()
+
+    def hide(self):
+        self.grab_release()
+        self.withdraw()
+
+
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -52,8 +109,8 @@ class Application(tk.Tk):
         self.config(menu=self._create_main_menu())
 
         paned_h = ttk.PanedWindow(self, orient=tk.HORIZONTAL)
-        paned_h.add(self._create_channel_manager(paned_h), weight=1)
-        paned_h.add(self._create_news_viewer(paned_h), weight=3)
+        paned_h.add(self._create_channel_manager(paned_h), weight=0)
+        paned_h.add(self._create_news_viewer(paned_h), weight=1)
         paned_h.pack(fill=tk.BOTH, expand=True)
 
         self._db_connection = sqlite3.connect(Path.home() / '.config' / 'news' / 'news.sqlite3')
@@ -61,6 +118,7 @@ class Application(tk.Tk):
         self._db_cursor = self._db_connection.cursor()
         # self._db_cursor.execute('pragma journal_mode=wal')
 
+        self._wait_dlg = WaitDialog(self)
         self._current_news = None
 
         self._load_data()
@@ -86,7 +144,7 @@ class Application(tk.Tk):
         self._channel_manager_channels = {}
         self._channel_manager_folders = {}
 
-        frame = tk.Frame(master)
+        frame = tk.Frame(master, width=200)
         self._channel_manager_treeview = ttk.Treeview(frame)
         self._channel_manager_treeview.grid(row=0, column=0, sticky=tk.NSEW)
         scrollbar_v = tk.Scrollbar(frame, orient=tk.VERTICAL)
@@ -158,10 +216,16 @@ class Application(tk.Tk):
         pass
 
     def _on_streamlink_360p(self, event=None):
-        pass
+        if self._current_news:
+            self._wait_dlg.show()
+            v = StreamlinkViewer(self._current_news['url'], '360p', on_start=self._wait_dlg.hide)
+            v.start()
 
     def _on_streamlink_worst(self, event=None):
-        pass
+        if self._current_news:
+            self._wait_dlg.show()
+            v = StreamlinkViewer(self._current_news['url'], 'worst', on_start=self._wait_dlg.hide)
+            v.start()
 
     def _on_goto_news(self, event=None):
         if self._current_news:
