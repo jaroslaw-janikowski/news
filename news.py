3ef2f7303163095e8e49b59d19190653f5ed8596
JJ <jaroslaw.janikowski@gmail.com>
2021-05-14 09:41:00 +0200

    Dodanie wybierania kolejnego newsa.

--- news.py
+++ news.py
@@ -2,22 +2,10 @@
 
 import sqlite3
 from pathlib import Path
+import webbrowser
 import tkinter as tk
 from tkinter import ttk
-
-
-class NewsList(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self._treeview = ttk.Treeview(self)
-        self._treeview.pack(fill=tk.BOTH, expand=True)
-
-
-class NewsViewer(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self._text = tk.Text(self)
-        self._text.pack(fill=tk.BOTH, expand=True)
+from tkinter.scrolledtext import ScrolledText
 
 
 class Application(tk.Tk):
@@ -30,25 +18,16 @@ class Application(tk.Tk):
         self.config(menu=self._create_main_menu())
 
         paned_h = ttk.PanedWindow(self, orient=tk.HORIZONTAL)
-        paned_v = ttk.PanedWindow(paned_h, orient=tk.VERTICAL)
-
-        self._create_channel_manager(paned_h)
-        self._news_list = NewsList(paned_v)
-        self._news_viewer = NewsViewer(paned_v)
-
-        # podział pomiędzy listą newsów kanału a przeglądarką
-        paned_v.add(self._news_list, weight=1)
-        paned_v.add(self._news_viewer, weight=3)
-        paned_v.pack(fill=tk.X, expand=True)
-
-        # poziomy podział okna
         paned_h.add(self._create_channel_manager(paned_h), weight=1)
-        paned_h.add(paned_v, weight=3)
+        paned_h.add(self._create_news_viewer(paned_h), weight=3)
         paned_h.pack(fill=tk.BOTH, expand=True)
 
         self._db_connection = sqlite3.connect(Path.home() / '.config' / 'news' / 'news.sqlite3')
         self._db_connection.row_factory = sqlite3.Row
         self._db_cursor = self._db_connection.cursor()
+        self._db_cursor.execute('pragma journal_mode=wal')
+
+        self._current_news = None
 
         self._load_data()
 
@@ -56,7 +35,9 @@ class Application(tk.Tk):
         # dodaj foldery
         folders = {}
         for row in self._db_cursor.execute('select * from folder').fetchall():
-            folders[row['id']] = self._channel_manager_treeview.insert('', tk.END, text=row['title'])
+            i = self._channel_manager_treeview.insert('', tk.END, text=row['title'])
+            folders[row['id']] = i
+            self._channel_manager_folders[row['title']] = i
 
         # dodaj kanały
         self._db_cursor.execute('select * from channel')
@@ -64,11 +45,22 @@ class Application(tk.Tk):
             parent_id = ''
             if row['folder_id'] is not None:
                 parent_id = folders[row['folder_id']]
-            self._channel_manager_treeview.insert(parent_id, tk.END, row['id'], text=row['title'])
+            i = self._channel_manager_treeview.insert(parent_id, tk.END, row['id'], text=row['title'])
+            self._channel_manager_channels[row['title']] = i
 
     def _create_channel_manager(self, master):
+        self._channel_manager_channels = {}
+        self._channel_manager_folders = {}
         self._channel_manager_treeview = ttk.Treeview(master)
         return self._channel_manager_treeview
+
+    def _create_news_viewer(self, master):
+        frame = tk.Frame(master)
+        self._news_viewer_title = tk.Label(frame)
+        self._news_viewer_title.pack(anchor=tk.NW, fill=tk.X, expand=False)
+        self._news_viewer_text = ScrolledText(frame)
+        self._news_viewer_text.pack(fill=tk.BOTH, expand=True)
+        return frame
 
     def _create_main_menu(self):
         menubar = tk.Menu(self, tearoff=False)
@@ -90,13 +82,13 @@ class Application(tk.Tk):
 
         news_menu = tk.Menu(self, tearoff=False)
         news_menu.add_command(label='Next', command=self._on_next_news, accelerator='N')
-        self.bind('<N>', self._on_next_news)
+        self.bind('n', self._on_next_news)
         news_menu.add_command(label='Goto', command=self._on_goto_news, accelerator='G')
-        self.bind('<G>', self._on_goto_news)
+        self.bind('g', self._on_goto_news)
         news_menu.add_command(label='Streamlink worst', command=self._on_streamlink_worst, accelerator='1')
-        self.bind('<1>', self._on_streamlink_worst)
+        self.bind('1', self._on_streamlink_worst)
         news_menu.add_command(label='Streamlink 360p', command=self._on_streamlink_360p, accelerator='2')
-        self.bind('<2>', self._on_streamlink_360p)
+        self.bind('2', self._on_streamlink_360p)
         news_menu.add_separator()
         news_menu.add_command(label='Mark all as read', command=self._on_mark_all_as_read, accelerator='Ctrl-M')
         self.bind('<Control-m>', self._on_mark_all_as_read)
@@ -122,10 +114,33 @@ class Application(tk.Tk):
         pass
 
     def _on_goto_news(self, event=None):
-        pass
+        if self._current_news:
+            webbrowser.open_new_tab(self._current_news['url'])
 
     def _on_next_news(self, event=None):
-        pass
+        # oznacz poprzedni aktywny news jako przeczytany
+        if self._current_news:
+            self._db_cursor.execute('update news set is_read = 1 where id = ?', (self._current_news['id'],))
+
+        # znajdź kolejny news
+        news = self._db_cursor.execute('select news.*, channel.title as channel_title from news join channel on channel.id = news.channel_id where is_read = 0 order by quality desc limit 1').fetchone()
+
+        self._current_news = news
+
+        if news is None:
+            return 'break'
+
+        self._set_news(news)
+
+    def _set_news(self, news):
+        '''Wybiera news jako aktywny w programie. Parametr to słownik z bazy z tabeli news.'''
+        # zaznacz i pokaż kanał w drzewie kanałów
+        self._channel_manager_treeview.selection_set(self._channel_manager_channels[news['channel_title']])
+
+        # załaduj treść do przeglądarki
+        self._news_viewer_text.delete('1.0', tk.END)
+        self._news_viewer_text.insert(tk.END, news['summary'])
+        self._news_viewer_title['text'] = news['title']
 
     def _on_add_folder(self, event=None):
         pass
