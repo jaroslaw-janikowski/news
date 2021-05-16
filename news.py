3c4904cb1ece0fd25d4ebaef507de725fefe7a46
JJ <jaroslaw.janikowski@gmail.com>
2021-05-16 19:25:28 +0200

    Naprawienie dialogu dodawania nowego kanału.

--- news.py
+++ news.py
@@ -89,17 +89,15 @@ class ChannelDialog(tk.Toplevel):
         self._result = False  # naciśnięto escape lub cancel
 
         tk.Label(self, text='Channel title').pack(fill=tk.X, padx=2)
-        self._channel_title = tk.StringVar()
-        self._channel_title.trace('w', self._validate)
-        channel_title_entry = tk.Entry(self, textvariable=self._channel_title)
-        channel_title_entry.pack(fill=tk.X, padx=2)
-        channel_title_entry.focus()
+        self._channel_title_entry = tk.Entry(self)
+        self._channel_title_entry.bind('<FocusOut>', self._validate)
+        self._channel_title_entry.pack(fill=tk.X, padx=2)
+        self._channel_title_entry.focus()
 
         tk.Label(self, text='URL').pack(fill=tk.X, padx=2)
-        self._channel_url = tk.StringVar()
-        self._channel_url.trace('w', self._validate)
-        channel_url_entry = tk.Entry(self)
-        channel_url_entry.pack(fill=tk.X, padx=2)
+        self._channel_url_entry = tk.Entry(self)
+        self._channel_url_entry.bind('<FocusOut>', self._validate)
+        self._channel_url_entry.pack(fill=tk.X, padx=2)
 
         button_cancel = tk.Button(self, text='Cancel', command=self._on_cancel)
         button_cancel.pack(fill=tk.X, padx=2)
@@ -111,54 +109,55 @@ class ChannelDialog(tk.Toplevel):
         self._autopopulate_form()
 
         b = all([
-            self._channel_title.get(),
-            self._channel_url.get()
+            self._channel_title_entry.get(),
+            self._channel_url_entry.get()
         ])
 
         self._button_ok['state'] = (tk.NORMAL if b else tk.DISABLED)
 
     def _autopopulate_form(self):
-        url = self._channel_title.get()
+        url = self._channel_title_entry.get()
 
         # link do kanału youtube
         m = re.match(r'^https:\/\/www\.youtube\.com\/channel\/([a-zA-Z0-9_\-]+)$', url)
         if m is not None:
             channel_id = m.group(1)
-            self._rss_url_entry.set_text(f'https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}')
-            self._rss_title_entry.set_text(channel_id)
+            self._channel_url_entry.delete(1, tk.END)
+            self._channel_url_entry.insert(tk.END, f'https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}')
+            self._channel_title_entry.delete(1, tk.END)
+            self._channel_title_entry.insert(tk.END, channel_id)
             return True
 
         # link do usera youtube
         m = re.match(r'^https:\/\/www\.youtube\.com\/user\/([a-zA-Z0-9_\-]+)$', url)
         if m is not None:
             user_id = m.group(1)
-            self._rss_url_entry.set_text(f'https://www.youtube.com/feeds/videos.xml?user={user_id}')
-            self._rss_title_entry.set_text(user_id)
+            self._channel_url_entry.delete(1, tk.END)
+            self._channel_url_entry.insert(tk.END, f'https://www.youtube.com/feeds/videos.xml?user={user_id}')
+            self._channel_title_entry.delete(1, tk.END)
+            self._channel_title_entry.insert(tk.END, user_id)
             return True
 
     def _on_cancel(self, event=None):
-        self._result = False
+        self._result = None
         self.destroy()
 
     def _on_apply(self, event=None):
-        self._result = True
+        self._result = {
+            'title': self._channel_title_entry.get(),
+            'url': self._channel_url_entry.get()
+        }
         self.destroy()
 
     def set_data(self, channel):
-        self._channel_title.set(channel['title'])
-        self._channel_url.set(channel['url'])
-
-    def get_data(self):
-        return {
-            'title': self._channel_title.get(),
-            'url': self._channel_url.get()
-        }
+        self._channel_title_entry.set(channel['title'])
+        self._channel_url_entry.set(channel['url'])
 
     def run(self):
         self.grab_set()
         self.wait_window()
         self.grab_release()
-        return self.get_data() if self._result else None
+        return self._result
 
 
 class ProgressDialog(tk.Toplevel):
@@ -458,10 +457,9 @@ class Application(tk.Tk):
 
     def _on_add_channel(self, event=None):
         dlg = ChannelDialog(self)
-        if dlg.run():
-            channel = dlg.get_data()
-
-            self._db_cursor.execute('insert into channel(title, url) values (?, ?)', (channel['title'], channel['url']))
+        data = dlg.run()
+        if data:
+            self._db_cursor.execute('insert into channel(title, url) values (?, ?)', (data['title'], data['url']))
 
             icon = 'rss'
             if 'youtube' in data['url']:
@@ -469,7 +467,7 @@ class Application(tk.Tk):
             elif 'twitch' in data['url']:
                 icon = 'twitch'
 
-            self._channel_manager_channels[channel['title']] = self._channel_manager_treeview.insert('', tk.END, text=channel['title'], image=self._icons[icon])
+            self._channel_manager_channels[data['title']] = self._channel_manager_treeview.insert('', tk.END, text=data['title'], image=self._icons[icon])
 
         dlg.destroy()
 
